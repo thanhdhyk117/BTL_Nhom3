e027d4ea7145fb19b2b16cd17c690ad968e408ad
thanhdhyk117 <63649787+thanhdhyk117@users.noreply.github.com>
2022-12-08 09:13:56 +0700

    Xoa file boi Nguyen Trung Thanh

--- admin/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define('DB_NAME', '_7octobre');
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_HOST', 'localhost');
-    define('TITLE', 'sitetitle');
-?>
